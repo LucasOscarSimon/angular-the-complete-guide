6f002719d78c71e1710fe6fceab828e0984281d3
Lucas Oscar Simon <lucas.oscar.simon@gmail.com>
2018-05-27 21:25:41 -0300

    All before Two-Way-Binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import { ServersComponent } from './servers/servers.component';
+import { NgModel} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { ServersComponent } from './servers/servers.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgModel
   ],
   providers: [],
   bootstrap: [AppComponent]
